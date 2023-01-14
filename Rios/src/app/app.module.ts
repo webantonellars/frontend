0ae0fb321a662f560fa8333fb6d8a9c56698c61c
webantonellars <antonellarios979@gmail.com>
2023-01-14 19:43:20 -0300

    septimo

--- Rios/src/app/app.module.ts
+++ Rios/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercademiComponent } from './components/acercademi/acercademi.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 
 
 @NgModule({
@@ -16,11 +21,16 @@ import { BannerComponent } from './components/banner/banner.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
+    AcercademiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+
    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
