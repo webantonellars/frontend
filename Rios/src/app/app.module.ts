afc65b3eecaceeeea86632a4ebb06bfd2cf51247
webantonellars <antonellarios979@gmail.com>
2023-01-14 01:13:16 -0300

    tercer

--- Rios/src/app/app.module.ts
+++ Rios/src/app/app.module.ts
@@ -4,33 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SobreMiComponent } from './components/sobremi/sobremi.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LogoApComponent } from './logo-ap/logo-ap.component';
-import { BannerComponent } from './components/banner/banner.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SobremiComponent,
+    LogoAPComponent,
+    BannerComponent,
+    SobreMiComponent,
     ExperienciaComponent,
     EducacionComponent,
     SkillsComponent,
     ProyectoComponent,
-    FooterComponent,
-    LogoApComponent,
-    BannerComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
